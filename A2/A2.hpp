bf5ed37dd84c501323b66f747395c3b24e2e43da
Matthew <m45yang@uwaterloo.ca>
2017-05-27 23:10:51 -0400

    switch indentation to spaces

--- A2/A2.hpp
+++ A2/A2.hpp
@@ -17,74 +17,74 @@ const GLsizei kMaxVertices = 1000;
 // Data should be copied over to GPU memory via VBO storage before rendering.
 class VertexData {
 public:
-	VertexData();
+  VertexData();
 
-	std::vector<glm::vec2> positions;
-	std::vector<glm::vec3> colours;
-	GLuint index;
-	GLsizei numVertices;
+  std::vector<glm::vec2> positions;
+  std::vector<glm::vec3> colours;
+  GLuint index;
+  GLsizei numVertices;
 };
 
 
 class A2 : public CS488Window {
 public:
-	A2();
-	virtual ~A2();
+  A2();
+  virtual ~A2();
 
 protected:
-	virtual void init() override;
-	virtual void appLogic() override;
-	virtual void guiLogic() override;
-	virtual void draw() override;
-	virtual void cleanup() override;
+  virtual void init() override;
+  virtual void appLogic() override;
+  virtual void guiLogic() override;
+  virtual void draw() override;
+  virtual void cleanup() override;
 
-	virtual bool cursorEnterWindowEvent(int entered) override;
-	virtual bool mouseMoveEvent(double xPos, double yPos) override;
-	virtual bool mouseButtonInputEvent(int button, int actions, int mods) override;
-	virtual bool mouseScrollEvent(double xOffSet, double yOffSet) override;
-	virtual bool windowResizeEvent(int width, int height) override;
-	virtual bool keyInputEvent(int key, int action, int mods) override;
+  virtual bool cursorEnterWindowEvent(int entered) override;
+  virtual bool mouseMoveEvent(double xPos, double yPos) override;
+  virtual bool mouseButtonInputEvent(int button, int actions, int mods) override;
+  virtual bool mouseScrollEvent(double xOffSet, double yOffSet) override;
+  virtual bool windowResizeEvent(int width, int height) override;
+  virtual bool keyInputEvent(int key, int action, int mods) override;
 
-	void createShaderProgram();
-	void enableVertexAttribIndices();
-	void generateVertexBuffers();
-	void mapVboDataToVertexAttributeLocation();
-	void uploadVertexDataToVbos();
-	void initializeModelCoordinates();
-	void initializeCoordinateFrames();
-	void initializeTransformationMatrices();
-	void applyModelTransformation();
-	void applyViewingTransformation();
-	void applyProjectionTransformation();
+  void createShaderProgram();
+  void enableVertexAttribIndices();
+  void generateVertexBuffers();
+  void mapVboDataToVertexAttributeLocation();
+  void uploadVertexDataToVbos();
+  void initializeModelCoordinates();
+  void initializeCoordinateFrames();
+  void initializeTransformationMatrices();
+  void applyModelTransformation();
+  void applyViewingTransformation();
+  void applyProjectionTransformation();
 
-	void initLineData();
+  void initLineData();
 
-	void setLineColour(const glm::vec3 & colour);
+  void setLineColour(const glm::vec3 & colour);
 
-	void drawLine (
-			const glm::vec2 & v0,
-			const glm::vec2 & v1
-	);
+  void drawLine (
+      const glm::vec2 & v0,
+      const glm::vec2 & v1
+  );
 
-	ShaderProgram m_shader;
+  ShaderProgram m_shader;
 
-	GLuint m_vao;            // Vertex Array Object
-	GLuint m_vbo_positions;  // Vertex Buffer Object
-	GLuint m_vbo_colours;    // Vertex Buffer Object
+  GLuint m_vao;            // Vertex Array Object
+  GLuint m_vbo_positions;  // Vertex Buffer Object
+  GLuint m_vbo_colours;    // Vertex Buffer Object
 
-	// Matrices controlling the camera and projection.
-	glm::mat3 t_proj;
-	glm::mat3 t_view;
-	glm::mat3 t_model;
+  // Matrices controlling the camera and projection.
+  glm::mat3 t_proj;
+  glm::mat3 t_view;
+  glm::mat3 t_model;
 
-	VertexData m_vertexData;
+  VertexData m_vertexData;
 
-	glm::vec3 m_currentLineColour;
-	std::vector<glm::vec3> model_coordinates;
-	std::vector<glm::vec3> world_coordinates;
-	std::vector<glm::vec3> view_coordinates;
+  glm::vec3 m_currentLineColour;
+  std::vector<glm::vec3> model_coordinates;
+  std::vector<glm::vec3> world_coordinates;
+  std::vector<glm::vec3> view_coordinates;
 
-	std::vector<glm::vec3> f_model;
-	std::vector<glm::vec3> f_world;
-	std::vector<glm::vec3> f_view;
+  std::vector<glm::vec3> f_model;
+  std::vector<glm::vec3> f_world;
+  std::vector<glm::vec3> f_view;
 };
